8e84b11021c6a5c07129efd45571e5111ce15b00
wser <w.web.dev@gmail.com>
2023-06-16 09:12:23 +0200

    3 files modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import deno from '@astrojs/deno';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: deno(),
+  output: 'server',
+  adapter: deno(),
+  integrations: [partytown()]
 });
